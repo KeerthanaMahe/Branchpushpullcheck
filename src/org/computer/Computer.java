c141ccbfd00bbde847ff626226693ed4bfaf935b
keerthanaMahe <keerthanamarimuthu63@gmail.com>
2021-03-25 08:10:12 +0530

    push file

---
+++ src/org/computer/Computer.java
@@ -0,0 +1,12 @@
+package org.computer;
+
+public class Computer {
+	private void computerModel() {
+		System.out.println("hp elitebook");
+	}
+ public static void main(String[] args) {
+	Computer c=new Computer();
+	c.computerModel();
+
+}
+}
